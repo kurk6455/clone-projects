4a7e574c3871f2ed44773949e8f60c4bf50068a7
kurk6455 <kurk6455@gmail.com>
2025-10-18 23:27:00 +0530

    basic user homepage setup

---
+++ mern-uber-clone/Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,42 @@
+import 'remixicon/fonts/remixicon.css'
+
+const LocationSearchPanel = () => {
+    return (
+        <>
+            <div className='flex border-b border-gray-200 pb-6'>
+                <div className='text-center px-3 py-2 rounded-full bg-gray-200 m-2 mt-3'>
+                    <i class="ri-map-pin-2-fill"></i>
+                </div>
+                <div>
+                    <div className='text-md font-medium'>Third Wave Coffee</div>
+                    <div className='text-xs'>Sarjapur - Marathahalli Road, Countryside</div>
+                    <div className='text-xs'>Layout, Rainbow Drive, Bengaluru, Karnataka, ...</div>
+                </div>
+            </div>
+
+            <div className='flex mt-4 border-b border-gray-200 pb-6'>
+                <div className='text-center px-3 py-2 rounded-full bg-gray-200 m-2 mt-3'>
+                    <i className="ri-map-pin-2-fill"></i>
+                </div>
+                <div>
+                    <div className='text-md font-medium'>Third Wave Coffee</div>
+                    <div className='text-xs'>Sarjapur - Marathahalli Road, Countryside</div>
+                    <div className='text-xs'>Layout, Rainbow Drive, Bengaluru, Karnataka, ...</div>
+                </div>
+            </div>
+
+            <div className='flex mt-4 border-b border-gray-200 pb-6'>
+                <div className='text-center px-3 py-2 rounded-full bg-gray-200 m-2 mt-3'>
+                    <i class="ri-map-pin-2-fill"></i>
+                </div>
+                <div>
+                    <div className='text-md font-medium'>Third Wave Coffee</div>
+                    <div className='text-xs'>Sarjapur - Marathahalli Road, Countryside</div>
+                    <div className='text-xs'>Layout, Rainbow Drive, Bengaluru, Karnataka, ...</div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LocationSearchPanel      
--- mern-uber-clone/Frontend/src/pages/Home.jsx
+++ mern-uber-clone/Frontend/src/pages/Home.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import uberLogo from '../assets/uberLogo.svg'
+import mapImage from '../assets/mapImage.png'
+import { useRef, useState } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import 'remixicon/fonts/remixicon.css'
+import LocationSearchPanel from '../components/LocationSearchPanel'
+
 
 const Home = () => {
+  const [pickup, setPickup] = useState('');
+  const [destination, setDestination] = useState('');
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+
+  useGSAP(() => {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: '73%',
+        padding: '20px'
+      })
+    } else {
+      gsap.to(panelRef.current, {
+        height: '0',
+        padding: '0px'
+      })
+    }
+  }, [panelOpen])
+
+
   return (
-    <div>Home</div>
+    <div className='relative h-screen overflow-hidden'>
+      {/* Background map image */}
+      <div className='absolute h-screen w-screen'>
+        <img src={mapImage} />
+      </div>
+
+      {/* Uber Logo */}
+      <div className='absolute top-0 left-3'>
+        <img src={uberLogo} className='w-15' />
+      </div>
+
+      <div className='opacity flex flex-col justify-end absolute top-0 h-screen w-full'>
+        {/* Find a trip (panel) */}
+        <div className='bg-white p-4 h-[27%]'>
+          {!panelOpen ? <div className='text-lg font-medium antialiased my-1'>Find a trip</div>
+            : <div className='text-lg font-medium antialiased my-1' onClick={() => setPanelOpen(false)}>
+              <i className="ri-arrow-down-s-line"></i>
+            </div>}
+
+          <input
+            type="text"
+            placeholder='Add a pick-up location'
+            onClick={() => setPanelOpen(true)}
+            value={pickup}
+            onChange={(e) => setPickup(e.target.value)}
+            className='bg-[#eeeeee] border-slate-300 text-sm my-2 w-full h-10 p-3 rounded indent-10'
+          />
+          <input
+            type="text"
+            placeholder='Enter your destination'
+            onClick={() => setPanelOpen(true)}
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            className='bg-[#eeeeee] border-slate-300 text-sm my-2 w-full h-10 p-3 rounded indent-8'
+          />
+        </div>
+
+        {/* location option */}
+        <div className='bg-white' ref={panelRef}>
+            <LocationSearchPanel />
+        </div>
+      </div>
+    </div>
   )
 }
 
